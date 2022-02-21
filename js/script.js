8265e3cd9637b99eba76062e28d9575fc2dad7d1
Manveer Kasseran <Manveer399@users.noreply.github.com>
2022-02-21 20:51:13 +0800

    skills section trial 1

--- js/script.js
+++ js/script.js
@@ -33,3 +33,35 @@ closeButton.addEventListener('click', () => {
   menu.classList.toggle('active')
   closeButton.classList.toggle('active')
 })
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+
+const closeModalButton = document.querySelectorAll('[data-close-button]')
+
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+closeModalButton.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
